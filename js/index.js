d382bcff37f8d084f3416c0cc5322f33c7029a5a
Dimpy089 <dimpyrewari06@gmail.com>
2025-07-16 21:11:03 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,7 +25,11 @@ mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology: true
 }).then(()=>{
     console.log('Connect to MongoDB successfully');
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT;
+    if (!port) {
+        console.error('Error: PORT environment variable not set.');
+        process.exit(1);
+    }
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
